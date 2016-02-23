005cde4d6657834a0fa8e042bbe80fc83292d824
Benjamin Low <lthben@gmail.com>
2016-02-23 19:01:13 +0800

    commit

--- accel_simulator.ino
+++ accel_simulator.ino
@@ -4,27 +4,32 @@ float x_val, y_val;
 long prev_tick;
 
 void setup() {
-        
-        Serial.begin(9600);
-        
+
+  Serial.begin(9600);
+
 }
 
 void loop() {
-        
-        if (millis() - prev_tick > UPDATE_RATE) {
-                 generate_data_pair(); 
-                   prev_tick = millis();      
-        }
 
+  if (millis() - prev_tick > UPDATE_RATE) {
+    generate_data_pair();
+    prev_tick = millis();
+  }
 }
 
 void generate_data_pair() {
-        
-         x_val += random(-10.0,10.0);
-         y_val += random(-10.0,10.0);
-   
-           Serial.println("a string");
-//            Serial.print(",");
-//             Serial.print(y_val);
-//              Serial.println();       
+
+  x_val += random(-10.0, 10.0);
+  y_val += random(-10.0, 10.0);
+
+  x_val = constrain(x_val, -90.0, 90.0);
+  y_val = constrain(y_val, -90.0, 90.0);
+
+  Serial.print(x_val);
+  Serial.print(",");
+  Serial.print(y_val);
+  Serial.println();
 }
+
+
+
